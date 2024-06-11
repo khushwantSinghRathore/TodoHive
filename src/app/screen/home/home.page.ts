3f53f54c6a1e817d9866e208cd9098304e1c5512
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-06-11 11:19:27 +0530

    home design update

--- src/app/screen/home/home.page.ts
+++ src/app/screen/home/home.page.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  categories = [
+    { icon: 'copy', name: 'Projects', num: 5, color: '#B4C4FF' },
+    { icon: 'car', name: 'Work', num: 3, color: '#CFF3E9' },
+    { icon: 'barbell', name: 'Daily Tasks', num: 2, color: '#9747FF' },
+    { icon: 'bag', name: 'Groceries', num: 7, color: '#EDBE7D' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
